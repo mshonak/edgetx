c9c9ae72a647544b5647629ecf9ae049864e737e
raphaelcoeffic <raphael.coeffic@frafos.com>
2023-01-02 07:21:57 +0100

    Add boxer ADC_DIRECTION

--- radio/src/targets/taranis/hal.h
+++ radio/src/targets/taranis/hal.h
@@ -1453,6 +1453,8 @@
   #define ADC_DIRECTION {-1,1,-1,1,  -1,-1,  1,  1}
 #elif defined(RADIO_TX12MK2)
   #define ADC_DIRECTION {-1,1,-1,1,  1,-1}
+#elif defined(RADIO_BOXER)
+  #define ADC_DIRECTION {1,-1,1,-1, -1,-1, 1}
 #elif defined(RADIO_ZORRO)
   #define ADC_DIRECTION {-1, 1, 1, -1, -1, 1, 1, 1}
 #elif defined(RADIO_TPRO)
