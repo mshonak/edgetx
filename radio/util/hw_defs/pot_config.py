1299d130938588b74ff3e42b4aeaa77afc34fe50
raphaelcoeffic <raphael.coeffic@frafos.com>
2023-02-28 17:34:31 +0100

    Fix default sliders

--- radio/util/hw_defs/pot_config.py
+++ radio/util/hw_defs/pot_config.py
@@ -52,19 +52,29 @@ POT_CONFIG = {
     },
     "x9d": {
         "P1": { "default": "WITH_DETENT" },
-        "P2": { "default": "WITH_DETENT" }
+        "P2": { "default": "WITH_DETENT" },
+        "SL1": { "default": "SLIDER_WITH_DETENT" },
+        "SL2": { "default": "SLIDER_WITH_DETENT" }
     },
     "x9dp": {
         "P1": { "default": "WITH_DETENT" },
-        "P2": { "default": "WITH_DETENT" }
+        "P2": { "default": "WITH_DETENT" },
+        "SL1": { "default": "SLIDER_WITH_DETENT" },
+        "SL2": { "default": "SLIDER_WITH_DETENT" }
     },
     "x9dp2019": {
         "P1": { "default": "WITH_DETENT" },
-        "P2": { "default": "WITH_DETENT" }
+        "P2": { "default": "WITH_DETENT" },
+        "SL1": { "default": "SLIDER_WITH_DETENT" },
+        "SL2": { "default": "SLIDER_WITH_DETENT" }
     },
     "x9e": {
         "P1": { "default": "WITH_DETENT" },
-        "P2": { "default": "WITH_DETENT" }
+        "P2": { "default": "WITH_DETENT" },
+        "SL1": { "default": "SLIDER_WITH_DETENT" },
+        "SL2": { "default": "SLIDER_WITH_DETENT" },
+        "SL3": { "default": "SLIDER_WITH_DETENT" },
+        "SL4": { "default": "SLIDER_WITH_DETENT" }
     },
     "x10": {
         "P1": { "default": "WITHOUT_DETENT" },
